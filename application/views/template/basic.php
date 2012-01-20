21422ea3173336e871a14e22beb99f7ebb904fdf
Javier Ayala <jav.forzza@gmail.com>
2012-01-20 11:44:38 -0600

    proyecto basico de codeigniter + elasticss + tank_auth

---
+++ application/views/template/basic.php
@@ -0,0 +1,7 @@
+<?php 
+	
+	$this->load->view('template/header');
+	
+	echo $contents;
+	
+	$this->load->view('template/footer');
---
+++ application/views/template/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es" xml:lang="es">
+<head>
+
+	<title>Welcome to CodeIgniter</title>
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> <?php // cambiar charset a iso-8859-1 para español?>
+		
+	<link rel="stylesheet" type="text/css" href="<?= base_url('elasticss/elastic.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('style.css')?>">
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" src="<?= base_url('elasticss/elastic.js')?>"></script>
+
+</head>
+<body>
